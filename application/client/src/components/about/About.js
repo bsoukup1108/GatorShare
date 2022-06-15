7faeaa0b4017a6523e52c1392d30ee9d37b88939
bsoukup1108 <bsoukup@mail.sfsu.edu>
2022-06-14 19:06:39 -0700

    about template

--- application/client/src/components/about/About.js
+++ application/client/src/components/about/About.js
@@ -5,9 +5,11 @@ import aleksPic from '../../img/alex.jpg';
 const About = () => {
 	return (
 		<Fragment>
-			<h1>About our team</h1>
+			<h1>CSC 648 Software Engineering<br></br>SFSU<br></br>Summer 2022<br></br>Section 01<br></br>Team 01</h1>
+			<h2>About the Team</h2>
 
 			<div class='accordion accordion-flush' id='accordionProfile'>
+				{/* PERSON #1  Team Leader*/}
 				<div class='accordion-item'>
 					<button
 						class='accordion-button collapsed'
@@ -18,8 +20,8 @@ const About = () => {
 						aria-controls='flush-collapseOne'
 					>
 						<h1>
-							<strong>Aleksandr</strong> – 
-							<i>Frontend developer</i>
+							<strong>NAME</strong> – 
+							<i>ROLE</i>
 						</h1>
 					</button>
 
@@ -29,12 +31,15 @@ const About = () => {
 						aria-labelledby='flush-headingOne'
 						data-bs-parent='#accordionProfile'
 					>
+						{/* upload your pic to the img folder
+					import it as in line 3
+					change the namee below */}
 						<img src={aleksPic} alt='pic' />
 
 						<div class='accordion-body'>blah blah blah</div>
 					</div>
 				</div>
-				{/* PERSON #2 */}
+				{/* PERSON #2 Backend Lead */}
 				<div class='accordion-item'>
 					<h2 class='accordion-header' id='flush-headingTwo'>
 						<button
@@ -64,16 +69,16 @@ const About = () => {
 						<div class='accordion-body'>DESCRIPTION</div>
 					</div>
 				</div>
-				{/* PERSON #3 */}
+				{/* PERSON #3 Frontend Lead*/}
 				<div class='accordion-item'>
-					<h2 class='accordion-header' id='flush-headingTwo'>
+					<h2 class='accordion-header' id='flush-headingThree'>
 						<button
 							class='accordion-button collapsed'
 							type='button'
 							data-bs-toggle='collapse'
-							data-bs-target='#flush-collapseTwo'
+							data-bs-target='#flush-collapseThree'
 							aria-expanded='false'
-							aria-controls='flush-collapseTwo'
+							aria-controls='flush-collapseThree'
 						>
 							<h1>
 								<strong>YOUR NAME</strong> – 
@@ -82,9 +87,9 @@ const About = () => {
 						</button>
 					</h2>
 					<div
-						id='flush-collapseTwo'
+						id='flush-collapseThree'
 						class='accordion-collapse collapse'
-						aria-labelledby='flush-headingTwo'
+						aria-labelledby='flush-headingThree'
 						data-bs-parent='#accordionProfile'
 					>
 						{/* upload your pic to the img folder
@@ -94,16 +99,16 @@ const About = () => {
 						<div class='accordion-body'>DESCRIPTION</div>
 					</div>
 				</div>
-				{/* PERSON #4 */}
+				{/* PERSON #4 Database master*/}
 				<div class='accordion-item'>
-					<h2 class='accordion-header' id='flush-headingTwo'>
+					<h2 class='accordion-header' id='flush-headingFour'>
 						<button
 							class='accordion-button collapsed'
 							type='button'
 							data-bs-toggle='collapse'
-							data-bs-target='#flush-collapseTwo'
+							data-bs-target='#flush-collapseFour'
 							aria-expanded='false'
-							aria-controls='flush-collapseTwo'
+							aria-controls='flush-collapseFour'
 						>
 							<h1>
 								<strong>YOUR NAME</strong> – 
@@ -112,9 +117,9 @@ const About = () => {
 						</button>
 					</h2>
 					<div
-						id='flush-collapseTwo'
+						id='flush-collapseFour'
 						class='accordion-collapse collapse'
-						aria-labelledby='flush-headingTwo'
+						aria-labelledby='flush-headingFour'
 						data-bs-parent='#accordionProfile'
 					>
 						{/* upload your pic to the img folder
@@ -124,16 +129,16 @@ const About = () => {
 						<div class='accordion-body'>DESCRIPTION</div>
 					</div>
 				</div>
-				{/* PERSON #5 */}
+				{/* PERSON #5 github master */}
 				<div class='accordion-item'>
-					<h2 class='accordion-header' id='flush-headingTwo'>
+					<h2 class='accordion-header' id='flush-headingFive'>
 						<button
 							class='accordion-button collapsed'
 							type='button'
 							data-bs-toggle='collapse'
-							data-bs-target='#flush-collapseTwo'
+							data-bs-target='#flush-collapseFive'
 							aria-expanded='false'
-							aria-controls='flush-collapseTwo'
+							aria-controls='flush-collapseFive'
 						>
 							<h1>
 								<strong>YOUR NAME</strong> – 
@@ -142,9 +147,9 @@ const About = () => {
 						</button>
 					</h2>
 					<div
-						id='flush-collapseTwo'
+						id='flush-collapseFive'
 						class='accordion-collapse collapse'
-						aria-labelledby='flush-headingTwo'
+						aria-labelledby='flush-headingFive'
 						data-bs-parent='#accordionProfile'
 					>
 						{/* upload your pic to the img folder
@@ -154,27 +159,27 @@ const About = () => {
 						<div class='accordion-body'>DESCRIPTION</div>
 					</div>
 				</div>
-				{/* PERSON #6 */}
+				{/* PERSON #6 Frontend*/}
 				<div class='accordion-item'>
-					<h2 class='accordion-header' id='flush-headingTwo'>
+					<h2 class='accordion-header' id='flush-headingSix'>
 						<button
 							class='accordion-button collapsed'
 							type='button'
 							data-bs-toggle='collapse'
-							data-bs-target='#flush-collapseTwo'
+							data-bs-target='#flush-collapseSix'
 							aria-expanded='false'
-							aria-controls='flush-collapseTwo'
+							aria-controls='flush-collapseSix'
 						>
 							<h1>
-								<strong>YOUR NAME</strong> – 
-								<i>ROLE</i>
+							<strong>Aleksandr</strong> – 
+							<i>Frontend developer</i>
 							</h1>
 						</button>
 					</h2>
 					<div
-						id='flush-collapseTwo'
+						id='flush-collapseSix'
 						class='accordion-collapse collapse'
-						aria-labelledby='flush-headingTwo'
+						aria-labelledby='flush-headingSix'
 						data-bs-parent='#accordionProfile'
 					>
 						{/* upload your pic to the img folder
