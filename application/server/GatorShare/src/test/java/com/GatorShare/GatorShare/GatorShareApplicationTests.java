2381dab3f8d41520142d134c43c04c87660c5703
Estefanos K <estefanosk@Estefanoss-MacBook-Pro.local>
2022-06-17 22:55:12 -0700

    added api endpoints for aboutus page

---
+++ application/server/GatorShare/src/test/java/com/GatorShare/GatorShare/GatorShareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GatorShare.GatorShare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatorShareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
