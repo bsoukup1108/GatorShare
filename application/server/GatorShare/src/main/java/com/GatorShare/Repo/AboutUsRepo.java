fddd6281d4d02dcd8c6e4be8683768ee4763a346
agusev <1990agusev@gmail.com>
2022-06-17 23:53:21 -0700

    a03502d4167de2de9730b57b34cfbe50b87f4e62

--- application/server/GatorShare/src/main/java/com/GatorShare/Repo/AboutUsRepo.java
+++
@@ -1,9 +0,0 @@
-package com.GatorShare.Repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.GatorShare.Dto.AboutUsDto;
-@Repository
-public interface AboutUsRepo  extends
-JpaRepository<AboutUsDto, Long> {
-}
