46f2d47c298b256e4de0430403b0e083df601bfe
agusev <1990agusev@gmail.com>
2022-07-18 16:58:09 -0700

    baseURL

--- application/client/src/http-common.js
+++ application/client/src/http-common.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 export default axios.create({
-	baseURL: 'http://localhost:1234/api',
+	baseURL: 'http://ec2-54-193-53-30.us-west-1.compute.amazonaws.com:5000/api',
 	headers: {
 		'Content-type': 'application/json',
 		'Access-Control-Allow-Origin': '*',
