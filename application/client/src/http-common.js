6e515081669cf8834120385089d5d62fc137f0fc
agusev <1990agusev@gmail.com>
2022-07-30 22:37:19 -0700

    for mobile view

--- application/client/src/http-common.js
+++ application/client/src/http-common.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 export default axios.create({
+	//baseURL: 'http://localhost:1234/api',
 	baseURL:
 		'http://ec2-54-193-53-30.us-west-1.compute.amazonaws.com:5000/api/',
 	headers: {
