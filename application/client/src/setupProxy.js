74df1db821c5071601b97e64a22f0a888d26136d
agusev <1990agusev@gmail.com>
2022-08-02 11:15:03 -0700

    404

--- application/client/src/setupProxy.js
+++ application/client/src/setupProxy.js
@@ -2,9 +2,8 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
 	app.use(
-		'/api',
 		createProxyMiddleware({
-			target: '/api/http://ec2-54-193-53-30.us-west-1.compute.amazonaws.com:5000/api/user/',
+			target: 'http://ec2-54-193-53-30.us-west-1.compute.amazonaws.com:5000/api/user/',
 			changeOrigin: true,
 		})
 	);
